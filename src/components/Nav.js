9328f49ac08bc71b1e6752798635036d2e41330f
Airijko <airijkoblade@gmail.com>
2024-04-30 06:28:22 -0500

    .

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -26,33 +26,39 @@ const Nav = () => {
     <div className="text-n-7 sticky top-0 w-full z-50">
       <div className="flex md:font-bold items-center px-5 md:px-7.5 xl:px-10 py-3 gap-5">
         <a className="flex items-center w-[12rem] md:mr-8" href="/">
-          <Image src="/assets/images/Webever_icon.png" alt="Logo" width={40} height={40} />
+          <Image
+            src="/assets/images/placeholder.png"
+            alt="Logo"
+            width={40}
+            height={40}
+          />
           <span className="hidden md:block font-code text-n-7 text-2xl uppercase md:text-md md:font-bold px-4 whitespace-nowrap">
             NextJS Template
           </span>
         </a>
 
         <nav
-          className={`${openNavigation ? "flex" : "hidden"
-            } fixed inset-0 md:static md:flex md:mx-auto w-full bg-n-1 `}
+          className={`${
+            openNavigation ? "flex" : "hidden"
+          } fixed inset-0 md:static md:flex md:mx-auto w-full bg-n-1 `}
         >
           <div className="relative z-2 flex flex-col items-center m-auto md:flex-row gap-[50px]">
             <a
-              href='/'
+              href="/"
               className={`block relative font-code text-2xl text-n-7 transition-colors hover:text-color-1 md:-mr-0.25 md:text-sm`}
               onClick={() => setOpenNavigation(false)}
             >
               Home
             </a>
             <a
-              href='/courses'
+              href="/courses"
               className={`block relative font-code text-2xl text-n-7 transition-colors hover:text-color-1 md:-mr-0.25 md:text-sm`}
               onClick={() => setOpenNavigation(false)}
             >
               Courses
             </a>
             <a
-              href='/about'
+              href="/about"
               className={`block relative font-code text-2xl text-n-7 transition-colors hover:text-color-1 md:-mr-0.25 md:text-sm`}
               onClick={() => setOpenNavigation(false)}
             >
@@ -107,7 +113,12 @@ const Nav = () => {
             className="flex items-center justify-center w-10 h-10"
             onClick={() => setOpenNavigation(!openNavigation)}
           >
-            <Image src="/assets/images/menu.svg" alt="Menu" width={40} height={40}></Image>
+            <Image
+              src="/assets/images/menu.svg"
+              alt="Menu"
+              width={40}
+              height={40}
+            ></Image>
           </button>
         </div>
       </div>
